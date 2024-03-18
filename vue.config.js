ab7a669e600851914345d3544cb24ac517289bb0
jeongminy <gks777777@gmail.com>
2024-03-18 17:35:28 +0900

    [Feat] main, login
    - 필요없는 주석제거

--- vue.config.js
+++ vue.config.js
@@ -8,16 +8,3 @@ module.exports = {
     port: 9090
   }
 }
-
-//로컬에서 테스트시 아래 코드 주석 풀면됨
-// const target = 'http://localhost:8080'
-// module.exports = {
-//   devServer: {
-//     port: 8080,
-//     proxy: {
-//       '^/api': {
-//       target,
-//       changeOrigin: true
-//     }
-//   }}
-// }
