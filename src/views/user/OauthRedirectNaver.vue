232b9b846f499de9ada685ca1090c70be7f669f0
jeongminy <gks777777@gmail.com>
2024-03-28 15:14:53 +0900

    [Feat] localstorage userId
    - 로그인시 로컬스토리지에 userId 전달

--- src/views/user/OauthRedirectNaver.vue
+++ src/views/user/OauthRedirectNaver.vue
@@ -25,6 +25,7 @@ export default {
         });
         // 일반 로그인 성공 시, 서버 응답으로부터 바로 토큰을 저장합니다.
         this.storeTokens(response.data.accessToken, response.data.refreshToken);
+        localStorage.setItem('userId', response.data.userId);
         this.$router.push('/');
         setTimeout(() => {
           window.location.reload()
